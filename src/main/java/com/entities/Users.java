ec4a26f8bb16393b5399e9972dda611eee50c8ef
Yaron Eidelman <dogkiller-e@hotmail.com>
2017-11-11 17:55:34 -0500

    small updates

--- src/main/java/com/entities/Users.java
+++ src/main/java/com/entities/Users.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name="user")
 public class Users {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
